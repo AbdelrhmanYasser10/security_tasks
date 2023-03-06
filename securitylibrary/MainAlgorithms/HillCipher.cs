fdc065fc7c8f6b9fcc8c99b43c9d750bd80dc438
AbdelrhmanYasser10 <61880662+AbdelrhmanYasser10@users.noreply.github.com>
2023-03-06 15:11:56 +0200

    first_commit

---
+++ securitylibrary/MainAlgorithms/HillCipher.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SecurityLibrary
+{
+    /// <summary>
+    /// The List<int> is row based. Which means that the key is given in row based manner.
+    /// </summary>
+    public class HillCipher :  ICryptographicTechnique<List<int>, List<int>>
+    {
+        public List<int> Analyse(List<int> plainText, List<int> cipherText)
+        {
+            for (int firstVal = 0; firstVal < 26; firstVal++)
+            {
+                for (int secondVal = 0; secondVal < 26; secondVal++)
+                {
+                    for (int thirdVal = 0; thirdVal < 26; thirdVal++)
+                    {
+                        for (int forthVal = 0; forthVal < 26; forthVal++)
+                        {
+                            List<int> tst_key = new List<int>(new[] { firstVal, secondVal, thirdVal, forthVal });
+                            List<int> enc_val = Encrypt(plainText, tst_key);
+                            bool check = true;
+                            int i = 0;
+                            foreach (int val in enc_val) {
+                                if (val != cipherText[i]) {
+                                    check = false;
+                                }
+                                i++;
+                            }
+                            if (check)
+                            {
+                                return tst_key;
+                            }
+
+                        }
+                    }
+                }
+            }
+
+            throw new InvalidAnlysisException();
+        }
+
+
+        public List<int> Decrypt(List<int> cipherText, List<int> key)
+        {
+            throw new NotImplementedException();
+        }
+
+
+        public List<int> Encrypt(List<int> plainText, List<int> key)
+        {
+            List<int> cipherValues = new List<int>();
+            int key_length = key.Count;
+            //mat 2x2
+            if (key_length == 4)
+            {
+                for (int i = 0; i < plainText.Count; i+= 2) {
+                    int first_val = (key[0] * plainText[i] + key[1] * plainText[i + 1]) % 26;
+                    int second_val = (key[2] * plainText[i] + key[3] * plainText[i + 1]) %26;
+                    cipherValues.Add(first_val);cipherValues.Add(second_val);
+                }
+            }
+            //mat 3x3
+            else {
+                for (int i = 0; i < plainText.Count; i+=3)
+                {
+                    int first_val = (key[0] * plainText[i] + key[1] * plainText[i + 1] + key[2] * plainText[i + 2]) % 26;
+                    int second_val = (key[3] * plainText[i] + key[4] * plainText[i + 1] + key[5] * plainText[i + 2]) % 26;
+                    int third_val = (key[6] * plainText[i] + key[7] * plainText[i + 1] + key[8] * plainText[i + 2]) % 26;
+                    cipherValues.Add(first_val); cipherValues.Add(second_val);cipherValues.Add(third_val);
+                }
+            }
+            return cipherValues;
+        }
+
+
+        public List<int> Analyse3By3Key(List<int> plainText, List<int> cipherText)
+        {
+            throw new NotImplementedException();
+        }
+
+    }
+}
---
+++ securitylibrary/MainAlgorithms/RailFence.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SecurityLibrary
+{
+    public class RailFence : ICryptographicTechnique<string, int>
+    {
+        public int Analyse(string plainText, string cipherText)
+        {
+            List<int> keyValues = new List<int>();
+            cipherText = cipherText.ToLower();
+            char charVal = cipherText.ElementAt(1);
+            int i = 0;
+            foreach (char val in plainText)
+            {
+                if (val == charVal)
+                    keyValues.Add(i);
+                i++;
+            }
+
+            foreach (var value in keyValues)
+            {
+                string s = Encrypt(plainText, value);
+                bool check = cipherText.Equals(s);
+                if (check)
+                {
+                    return value;
+                }
+            }
+
+            return -1;
+        }
+
+        public string Decrypt(string cipherText, int key)
+        {
+            string ans = "";
+            int ptLength = (int)Math.Ceiling((decimal)cipherText.Length / (decimal)key);
+            int[,] mat = new int[key,ptLength];
+            for (int i = 0; i < key; i++) {
+                for (int j = 0; j < ptLength; j++) {
+                    mat[i, j] = -1;
+                }
+            }
+            int index_of_char = 0;
+            for (int i = 0; i < key; i++) {
+                for (int j = 0; j < ptLength; j++) {
+                    if (index_of_char < cipherText.Length)
+                    {
+                        mat[i, j] = index_of_char;
+                        index_of_char++;
+                    }
+                }
+            }
+            for (int i = 0; i < ptLength; i++) {
+                for (int j = 0; j < key; j++) {
+                    if (mat[j, i] <= index_of_char && mat[j,i] != -1)
+                    {
+                        ans += cipherText[mat[j, i]];
+                    }
+                }
+            }
+            return ans;
+        }
+
+        public string Encrypt(string plainText, int key)
+        {
+            string trimmed = String.Concat(plainText.Where(c => !Char.IsWhiteSpace(c)));
+            string ans = "";
+            for (int j = 0; j < key; j++)
+            {
+                for (int i = j; i < trimmed.Length; i += key) {
+                    ans += trimmed[i];
+                }
+            }
+            Console.WriteLine(ans);
+            return ans;
+        }
+    }
+}
