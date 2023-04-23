4ec76fb347eb5cff00b3241976084572152a0399
AbdelrhmanYasser10 <61880662+AbdelrhmanYasser10@users.noreply.github.com>
2023-04-23 09:18:18 +0200

    finish m3

--- securitylibrary/ElGamal/ELGAMAL.cs
+++ securitylibrary/ElGamal/ELGAMAL.cs
@@ -16,15 +16,94 @@ namespace SecurityLibrary.ElGamal
         /// <param name="y"></param>
         /// <param name="k"></param>
         /// <returns>list[0] = C1, List[1] = C2</returns>
+        public static int CalcuOfPower(int x, int y)
+        {
+            if (y == 0)
+                return 1;
+            else
+                return x * CalcuOfPower(x, y - 1);
+        }
         public List<long> Encrypt(int q, int alpha, int y, int k, int m)
         {
-            throw new NotImplementedException();
+            List<long> result = new List<long>();
+            double K = (Math.Pow(y, k)) % q;
+            int tempRes;
+            int result1 = 1;
+            int result11 = 1;
+            for (int i = 0; i < k; i++)
+            {
+                tempRes = (CalcuOfPower(y, 1));
+                result1 = (tempRes % q) * (result1 % q);
+            }
+            result1 = result1 % q;
+            for (int i = 0; i < k; i++)
+            {
+                tempRes = (CalcuOfPower(alpha, 1));
+                result11 = (tempRes % q) * (result11 % q);
+            }
+            result11 = result11 % q;
+            long C1 = (long)(result11 % q);
+            long C2 = (long)result1 * m % q;
+            result.Add(C1);
+            result.Add(C2);
+            return result;
+            //throw new NotImplementedException();
 
         }
+
         public int Decrypt(int c1, int c2, int x, int q)
         {
-            throw new NotImplementedException();
+            int res_K = 1;
+            int pow_val = c1;
+            for (int b = x; b > 0; b /= 2) {
+                if (b % 2 == 1)
+                {
+                    res_K = (res_K * pow_val) % q;
+                }
+                pow_val = (pow_val * pow_val) % q;
+            }
+            res_K = res_K % q;
+            int inverseK = (int)extendedGCD(res_K, q);
+            int M = (c2 * inverseK) % q;
+            return M % q;
 
         }
+        #region[Extended GCD]
+        public static long extendedGCD(long a, long b)
+        {
+            long s_result = b;
+            //Swap values
+            if (a < b)
+            {
+                long tmp = a;
+                a = b;
+                b = tmp;
+            }
+            long R = b,
+                Q = 0,
+                X0 = 1,
+                Y0 = 0,
+                X1 = 0,
+                Y1 = 1,
+                X = 0,
+                Y = 0;
+            while (R > 1)
+            {
+                R = a % b;
+                Q = a / b;
+                X = (X0 - Q * X1);
+                Y = (Y0 - Q * Y1);
+                X0 = X1;
+                Y0 = Y1;
+                X1 = X;
+                Y1 = Y;
+                a = b;
+                b = R;
+            }
+            while (Y < 0)
+                Y += s_result;
+            return Y;
+        }
+        #endregion
     }
 }
--- securitylibrary/RSA/RSA.cs
+++ securitylibrary/RSA/RSA.cs
@@ -8,14 +8,57 @@ namespace SecurityLibrary.RSA
 {
     public class RSA
     {
+        public static int CalcuOfPower(int x, int y)
+        {
+            if (y == 0)
+                return 1;
+            else
+                return x * CalcuOfPower(x, y - 1);
+        }
+
+        public int ModMultInverse(int e, int eulerValue)
+        {
+            double inverse = 1;
+
+            for (int i = 1; i <= e; i++)
+            {
+                inverse = i * eulerValue;
+                inverse++;
+                inverse = inverse / e;
+                if (inverse % 1 == 0)
+                    break;
+            }
+
+            return (int)inverse;
+        }
+
         public int Encrypt(int p, int q, int M, int e)
         {
-            throw new NotImplementedException();
+            int n = p * q;
+            int result;
+            int result1 = 1;
+            for (int i = 1; i <= e; i++)
+            {
+                result = (CalcuOfPower(M, 1));
+                result1 = (result % n) * (result1 % n);
+            }
+            result1 = result1 % n;
+            return (int)result1;
         }
 
         public int Decrypt(int p, int q, int C, int e)
         {
-            throw new NotImplementedException();
+            int eluer = (p - 1) * (q - 1);
+            int final_res = 1;
+            int n = p * q;
+            int d = ModMultInverse(e, eluer);
+            for (int i = 0; i < d; i++)
+            {
+                final_res = final_res * C;
+                final_res = final_res % n;
+            }
+            //final_res = final_res % n;
+            return (int)final_res;
         }
     }
 }
