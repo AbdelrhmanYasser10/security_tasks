d97cafd8e9dbda9801d8abb5a12bbfc681cb8de9
AbdelrhmanYasser10 <61880662+AbdelrhmanYasser10@users.noreply.github.com>
2023-04-24 03:40:09 +0200

    finish 3rd milestone

--- securitylibrary/DiffieHellman/DiffieHellman.cs
+++ securitylibrary/DiffieHellman/DiffieHellman.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.Win32;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,11 +7,30 @@ using System.Threading.Tasks;
 
 namespace SecurityLibrary.DiffieHellman
 {
-    public class DiffieHellman 
+    public class DiffieHellman
     {
+        public int Pow(int a, int b, int q)
+        {
+            int result = 1;
+            for (int i = 0; i < b; i++)
+            {
+                result %= q;
+                result *= a;
+
+            }
+            return result % q;
+        }
         public List<int> GetKeys(int q, int alpha, int xa, int xb)
         {
-            throw new NotImplementedException();
+            List<int> output = new List<int>();
+            int ya = Pow(alpha, xa, q);
+            int yb = Pow(alpha, xb, q);
+            int K = Pow(yb, xa, q);
+            //I will add k of user A twice cause user A and B have the same key
+            output.Add(K);
+            output.Add(K);
+
+            return output;
         }
     }
 }
